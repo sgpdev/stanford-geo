891bf75e095a27ccbc87b3c9f6ce60345d6396a7
slattouf95 <34458665+slattouf95@users.noreply.github.com>
2018-12-22 13:31:13 -0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,9 @@
 const { Pool } = require("pg");
-// const express = require("express");
-// const app = express();
-//Check if they are needed or not
 
 const pool = new Pool({
-  user: "sufyan",
-  host: "localhost",
-  database: "geochem_test",
-  password: "",
+  user: "sgpserver",
+  host: "se3pgdb.stanford.edu",
+  database: "geochem1",
+  password: "sgpsgp",
   port: 5432
 });
-
-module.exports = {
-  pool
-};
