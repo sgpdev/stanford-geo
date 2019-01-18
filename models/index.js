22c738478bc89d6f531797b1202040e05b99a4dd
Rifaat Samawi <rsamawi@stanford.edu>
2019-01-18 15:44:53 -0800

    two lines

--- models/index.js
+++ models/index.js
@@ -8,10 +8,7 @@ const { Pool } = require("pg");
   port: 5432
 });*/
 
-<<<<<<< HEAD
 /*&ohXi$Vn*/
-=======
->>>>>>> 444e97da2a847c29811a9d5dd640ce927f826359
 const pool = new Pool({
   user: "sgp_server",
   host: "se3pgdb.stanford.edu",
