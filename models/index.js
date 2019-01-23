048014a11d67fe08f6a5d6c535e1782de417f1dc
Rifaat Samawi <rsamawi@stanford.edu>
2019-01-23 03:06:26 -0800

    fix for munsell code and turbiditic

--- models/index.js
+++ models/index.js
@@ -1,21 +1,21 @@
 const { Pool } = require("pg");
 
-/*const pool = new Pool({
+const pool = new Pool({
   user: "sgp_server",
   host: "localhost",
   database: "geochem_test",
   password: "sgpsgp",
   port: 5432
-});*/
+});
 
 /*&ohXi$Vn*/
-const pool = new Pool({
+/*const pool = new Pool({
   user: "sgp_server",
   host: "se3pgdb.stanford.edu",
   database: "geochem1",
   password: "sgpsgp",
   port: 5432
-});
+});*/
 
 module.exports = {
   pool
