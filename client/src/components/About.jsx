7c2893ed20fd24f219cc9a297ae52edcff11f77a
slattouf95 <lattoufsufian@gmail.com>
2019-01-22 17:09:08 -0800

    add analyte code ui

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -99,84 +99,16 @@ class About extends Component {
           <h3 className="title-doc">Collaborative Team</h3>
           <br />
           <br />
-          <ul>
-            <li> Aivo Lepland, Norwegian Geological Survey</li>
-            <li>Alan Rooney, Yale University</li>
-            <li>Alf Lenz, University of Western Ontario</li>
-            <li>Anais Pages, CSIRO</li>
-            <li>Andy Knoll, Harvard University</li>
-            <li>Anne-Sofie Ahm, University of Copenhagen</li>
-            <li>Austin Miller, Stanford University</li>
-            <li>Ben Gill, Virginia Tech</li>
-            <li>Brennan O'Connell, Yale University</li>
-            <li>Carol Dehler, Utah State University</li>
-            <li>Charles Henderson, University of Calgary</li>
-            <li>Chao Li, China University of Geosciences</li>
-            <li>Chengsheng Jin, China University of Geosciences</li>
-            <li>Clint Scott, USGS</li>
-            <li>Danielle Thomson, Shell</li>
-            <li>David Loydell, University of Portsmouth</li>
-            <li>Devon Cole, Yale University</li>
-            <li>Don Canfield, Southern Denmark University</li>
-            <li>Emma Hammarlund, Southern Denmark University</li>
-            <li>Emmy Smith, Johns Hopkins University</li>
-            <li>Florian Kurzweil, University of Tubingen</li>
-            <li>Francis Macdonald, Harvard University</li>
-            <li>Gabriel Uhlein, Universidade Federal de Minas Gerais</li>
-            <li>Galen Halverson, McGill University</li>
-            <li>Geoff Gilleaudeau, Arizona State University</li>
-            <li>Heda Agic, University of California, Santa Barbara</li>
-            <li>Huajian Wang, Key Laboratory of Petroleum Geochemistry</li>
-            <li>Inessa Yurchenko, Stanford University</li>
-            <li>Jianghai Yang, China University of Geosciences, Wuhan</li>
-            <li>Joe Shaffer, Wayfair</li>
-            <li>Jon Husson, University of Victoria</li>
-            <li>Keith Dewing, Geological Survey of Canada</li>
-            <li>Julie Dumoulin, USGS</li>
-            <li>Julien Kimmig, University of Kansas</li>
-            <li>Justin Strauss, Dartmouth College</li>
-            <li>Karin Goldberg, Kansas State University</li>
-            <li>Lawrence Och, Dr. von Moos AG</li>
-            <li>Lei Xiang, Nanjing Institute of Geology and Paleontology</li>
-            <li>Liam Bhajan, Stanford University</li>
-            <li>Malcolm Hodgskiss, Stanford University</li>
-            <li>Marcus Kunzmann, CSIRO</li>
-            <li>Matthew Clarkson, University of Otago</li>
-            <li>Meng Cheng, China University of Geosciences</li>
-            <li>Mike Melchin, St. Francis Xavier University</li>
-            <li>Patrick Sack, Yukon Geological Survey</li>
-            <li>
-              Paulo Linarde Dantas Mascena, Federal University of Rio Grande do
-              Norte
-            </li>
-            <li>Pavel Kabanov, Geological Survey of Canada</li>
-            <li>Phil Wilby, British Geological Survey</li>
-            <li>Rachel Wood, University of Edinburgh</li>
-            <li>Robert Gaines, Pomona College</li>
-            <li>Romain Guilbaud, University of Lancaster</li>
-            <li>Richard Stockey, Stanford University</li>
-            <li>Rifaat Samawi, Stanford University</li>
-            <li>Sabrina Tecklenburg, Stanford University</li>
-            <li>Sam Spinks, CSIRO</li>
-            <li>Samantha VanSickle, Stanford University</li>
-            <li>Shanan Peters, University of Wisconsin</li>
-            <li>Shane Schoepfer, University of Calgary</li>
-            <li>Simon Poulton, University of Leeds</li>
-            <li>
-              Stephanie Plaza-Torres, The University of Puerto Rico at Mayaguez
-            </li>
-            <li>Sylvain Richoz, University of Graz</li>
-            <li>Tais Dahl, University of Copenhagen</li>
-            <li>Tiffani Fraser, Yukon Geological Survey</li>
-            <li>Tim Gibson, Dartmouth College</li>
-            <li>Tina Woltz, University of California, Santa Barbara</li>
-            <li>Tom Boag, Stanford University</li>
-            <li>Una Farrell, Stanford University</li>
-            <li>Will Thompson-Butler, Stanford University</li>
-            <li>Wing Chan, University of Calgary</li>
-            <li>Xinze Lu, University of Waterloo</li>
-            <li>Yu Liu, China University of Geosciences</li>
-          </ul>
+          <div>
+            The heart of SGP is the collaborative team, who help provide
+            geological context data and geochemical data and participate in
+            Working Groups addressing particular questions about Earth’s
+            environmental evolution. A full list of collaborative team members
+            can be found on the SGP website:{" "}
+            <a target="_blank" href="https://sgp.stanford.edu/who-we-are">
+              sgp.stanford.edu
+            </a>
+          </div>
         </div>
       </span>
     );
