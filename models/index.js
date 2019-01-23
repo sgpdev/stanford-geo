92d2af79cc2d4dc0a276a78062c7242028f1544f
slattouf95 <lattoufsufian@gmail.com>
2019-01-22 17:36:08 -0800

    edit production env

--- models/index.js
+++ models/index.js
@@ -1,10 +1,18 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "sufyan",
+/*const pool = new Pool({
+  user: "sgp_server",
   host: "localhost",
   database: "geochem_test",
-  password: "",
+  password: "sgpsgp",
+  port: 5432
+});*/
+
+const pool = new Pool({
+  user: "sgpserver",
+  host: "se3pgdb.stanford.edu",
+  database: "geochem1",
+  password: "sgpsgp",
   port: 5432
 });
 
